a7261eb65b0140ad6a3f4e9c4e08d9cf8b2e479f
mateuscleite <mateus.kastilho@gmail.com>
2020-10-07 13:27:26 -0300

    Default page layout implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { PageDefaultComponent } from './components/page-default/page-default.component';
+import { ContentComponent } from './components/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    PageDefaultComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
